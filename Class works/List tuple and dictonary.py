6a395b32bdcbdafdeb2cfa92adf724286d99322d
Rohan Samudrala <rohansid0314@gmail.com>
2025-12-12 10:29:06 +0530

    folders created

--- Class works/List tuple and dictonary.py
+++ Class works/List tuple and dictonary.py
@@ -78,14 +78,85 @@ print(l.count(5))  #counting repitation of  a pirticular element
 '''
 
 '''
-l.sort()
+l.sort()  #it effects the original list
 print(l)'''
 
 '''
-print(sorted(l))'''
+print(sorted(l))    #it wont effect the original list
+''' 
 
 '''print(len(l))
 
 print(sum(a)) #only works when the list contining numeric values or float values
 any(a) #used to check if any values in the list are true or not
 all(a)  #used to check if all values in the list are true or not'''
+
+#tuple
+
+    #immutable, ordered collection of elements
+    #tuples are faseter thean lists because it is immutable
+  #t=()
+  #t=tuple()
+  #t=(1,2,3,4)
+  #t=(1,1.1,'string',[],(),{},{1,2,3})
+
+'''t=(1,2,3)
+s=(2,3,4)
+print(t+s)
+print(4 in t)
+print(3 in s)'''
+
+'''
+lang=('java','python','html','ruby','css')
+print(lang[2])
+print(lang[-3])
+
+print(lang[::])
+
+print(lang[:3])
+
+print(lang[::2])
+
+print(lang[-3::])
+
+print(lang[-3:-1:])
+
+print(lang[-3::-1])'''
+
+
+
+#packing and unpacking
+"""
+z=1,2,3,4       #default considered as tuple
+print(type(a))
+a,b,c,d=z
+print(d)
+
+user1='rohan','rohansiddusamudrala@zohomail.in','rohan000'
+uname,mail,password=user1
+print(uname,mail)
+print(f" User-name:\n\t{uname} \n mail: \n\t{mail} \n password: \n\t{password}")
+"""
+
+
+'''t=(1,2,[5,9],3)
+print(t[0])
+t[2].append(0)
+print(t)'''
+
+#Dictonary
+'''d={}
+d={1:2,2:3,3:4}
+print(2 in d) #works only for the keys 
+print(d[2])'''
+
+'''data={'name':'subhash','batchno':44,'course':'python full stack'}
+print(data['name'])
+print(data.get('age'))
+print(data.get('age',"Key went to tour"))'''
+
+#Set default search for the key if not there it appends the key to the dictonary
+'''data.setdefault('age',21)
+print(data)
+print(data.setdefault('name'))
+print(data)'''
